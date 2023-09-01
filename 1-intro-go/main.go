930efe997ba8afa1cad1b411d7ef9eb9292c8f3b
yukselbey35 <yukselbey35@gmail.com>
2023-09-01 08:46:59 -0700

    create go review files

---
+++ 1-intro-go/main.go
@@ -0,0 +1,9 @@
+// With Every go code start with package main so our program compile and run stand alone
+package main
+
+import "fmt" // fmt package we can print text to console
+
+// Entry of the go code/program
+func main() {
+	fmt.Println("Hello World-GO is fast")
+}
---
+++ 3-compile-compare-speed/readme.md
@@ -0,0 +1,3 @@
+# GO is one of the fast programming languages.(Python, JavaScript)  
+
+# COnverting humand readable code to binary is compiling the code, CPU or other PC items can not understand :-)
